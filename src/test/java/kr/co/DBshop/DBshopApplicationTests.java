1444bee197d60a18ac95cb242fb208ee029a2e25
YH-Ryu <tyuy1018@gmail.com>
2025-01-24 10:57:40 +0900

    기본설정

---
+++ src/test/java/kr/co/DBshop/DBshopApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.DBshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DBshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
