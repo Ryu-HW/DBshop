ae4efac40dec9ab4a5d61048d50542ae8cb3bdc3
박성철 <imf345666@gmail.com>
2025-01-24 15:03:07 +0900

    category create 성공 1버전.

--- src/main/java/kr/co/DBshop/controller/category/CategoryController.java
+++ src/main/java/kr/co/DBshop/controller/category/CategoryController.java
@@ -1,10 +1,36 @@
 package kr.co.DBshop.controller.category;
 
+import kr.co.DBshop.dto.category.CategoryDto;
+import kr.co.DBshop.service.category.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/category")
 public class CategoryController {
 
+    @Autowired
+    private CategoryService categoryService;
+
+//    @GetMapping("/hello")
+//    public String categoryHello(){
+//        return "hello";
+//    }
+
+    @GetMapping("/create")
+    public String create() {
+        return "category/create-category";
+    }
+
+    @PostMapping
+    public String createCategory(@ModelAttribute CategoryDto categoryDto) {
+            System.out.println(categoryDto.getCategory_name());
+            categoryService.createCategory(categoryDto);
+
+            return "/category/hello";
+        }
+    }
 
 
-}
+
